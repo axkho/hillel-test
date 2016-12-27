a2448f0cc5d67b279b8303943b46ecaa0c96dabc
Alexander Khomenko <alxkhom@gmail.com>
2016-12-27 16:40:42 +0200

    Removed global vars, clicked items change via classList

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
-/*Globals*/
-var nav;
-var data;
-var contentTemplate;
-var contentTemplateNodes = [];
-/**/
-
 document.addEventListener("DOMContentLoaded", function(event) { init(); });
 
 function init(){
-  nav = getElements('#nav>li');
-  data = window.myData;
-  contentTemplate = getElement('#contentTemplate').content;
-  
-  contentTemplateNodes = [
-    contentTemplate.querySelectorAll('.contentTitle')[0],
-    contentTemplate.querySelectorAll('.contentImg')[0],
-    contentTemplate.querySelectorAll('.contentText')[0]
-  ];
-
   indexnav();
   addListinersTonav();
   populateTemplate(0);
@@ -25,38 +8,44 @@ function init(){
 }
 
 function indexnav(){
-  if(nav)
-    for(var i=0; i<nav.length; i++)
-      nav[i].setAttribute("item-index", i);
+  if(getNav())
+    for(var i=0; i<getNav().length; i++)
+      getNav()[i].setAttribute("item-index", i);
 }
 
 function addListinersTonav() {
-  if(nav)
-    for(var i = 0; i<nav.length; i++)
-      nav[i].addEventListener('click', getContent);
+  if(getNav())
+    for(var i = 0; i<getNav().length; i++)
+      getNav()[i].addEventListener('click', getContent);
 }
 
 function getContent(event){
   var item = event.target;
   removeSelection();
-  item.className = 'clicked';
+  item.classList.add('clicked');
   populateTemplate(item.getAttribute('item-index'));
 }
 
 function removeSelection(){
-  for(var i=0; i<nav.length; i++)
-    nav[i].className = '';
+  for(var i=0; i<getNav().length; i++)
+    getNav()[i].classList.remove('clicked');
 }
 
 function populateTemplate(contentItem){
   resetTemplate();
-  contentTemplateNodes[0].innerHTML = data[contentItem].title;
-  contentTemplateNodes[1].setAttribute('src', data[contentItem].img);
-  contentTemplateNodes[1].setAttribute('alt', 'placeholder');
-  contentTemplateNodes[2].innerHTML = data[contentItem].text;
+
+  var template = getTemplate();
+  var title = template.querySelectorAll('.contentTitle')[0];
+  var img = template.querySelectorAll('.contentImg')[0];
+  var text = template.querySelectorAll('.contentText')[0];
+
+  title.innerHTML = window.myData[contentItem].title;
+  img.setAttribute('src', window.myData[contentItem].img);
+  img.setAttribute('alt', 'placeholder');
+  text.innerHTML = window.myData[contentItem].text;
 
   getElement('#content').appendChild(
-    document.importNode(contentTemplate, true));
+    document.importNode(template, true));
 }
 
 function resetTemplate(){
@@ -65,11 +54,17 @@ function resetTemplate(){
     content.removeChild(content.lastChild);
   }
 }
+/***/
 /*******/
 function getElement(selector) {
   return document.querySelector(selector);
 }
-
 function getElements(selector) {
   return document.querySelectorAll(selector);
+}
+function getNav() {
+  return getElements('#nav>li');
+}
+function getTemplate() {
+  return getElement('#contentTemplate').content;
 }
